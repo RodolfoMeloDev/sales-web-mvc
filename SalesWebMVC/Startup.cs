abe4933f12d00625164636b477c5a9b8a04d42bf
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-04-19 19:45:06 -0300

    Ajustado classe Department para usar migrations Ajustado connectionString para especificar o schema a ser usado Ajustado Startup para usar Migrations on projeto

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -28,7 +28,8 @@ namespace SalesWebMVC
             services.AddControllersWithViews();
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseNpgsql(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseNpgsql(Configuration.GetConnectionString("SalesWebMvcContext"), 
+                                      builder => builder.MigrationsAssembly("SalesWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
