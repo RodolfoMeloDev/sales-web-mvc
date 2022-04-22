0deaeab8c431df642bf5ea88a08c85264c9c2110
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-04-22 19:46:12 -0300

    Adicionado FK na tabela de Departamento

--- SalesWebMVC/Migrations/20220422224059_DepartmentIdForeignKey.Designer.cs
+++ SalesWebMVC/Migrations/20220422224059_DepartmentIdForeignKey.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SalesWebMvc.Data;
@@ -9,9 +10,10 @@ using SalesWebMvc.Data;
 namespace SalesWebMVC.Migrations
 {
     [DbContext(typeof(SalesWebMvcContext))]
-    partial class SalesWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20220422224059_DepartmentIdForeignKey")]
+    partial class DepartmentIdForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
